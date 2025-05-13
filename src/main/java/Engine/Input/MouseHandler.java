23201c13d29ab900697565b5e8045ca327bf14f8
Kyle <thorntonkylencd@gmail.com>
2025-05-13 12:40:42 +0100

    Added mouse drag / movement detection

--- src/main/java/Engine/Input/MouseHandler.java
+++ src/main/java/Engine/Input/MouseHandler.java
@@ -2,18 +2,26 @@ package Engine.Input;
 import Engine.GameWindow;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
-public class MouseHandler extends MouseAdapter {
+public class MouseHandler extends MouseAdapter implements MouseMotionListener {
     private int mouseX, mouseY;
     private boolean clicked;
 
     public MouseHandler(GameWindow gameWindow) {
         gameWindow.addMouseListener(this);
+        gameWindow.addMouseMotionListener(this);
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
         clicked = true;
+        mouseX = e.getX();
+        mouseY = e.getY();
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
         mouseX = e.getX();
         mouseY = e.getY();
     }
